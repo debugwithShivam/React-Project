3e791c449e78e1490d2f7c9c9ac4c8bde318d22a
shivam panday <sp5812070@gmail.com>
2026-09-01 20:48:30 +0530

    new Backend API

--- Notebook/frontend/src/config/backend_API_URL.js
+++ Notebook/frontend/src/config/backend_API_URL.js
@@ -1,4 +1,4 @@
-// const VITE_API_URL = 'https://react-project-jt2c.onrender.com'
-const VITE_API_URL = 'http://localhost:5000'
+const VITE_API_URL = 'https://react-project-jt2c.onrender.com'
+// const VITE_API_URL = 'http://localhost:5000'
 
 export default VITE_API_URL
